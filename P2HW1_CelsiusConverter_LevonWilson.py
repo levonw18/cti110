ef4554c69622c1fe752b7862e184929bac902833
levonw18 <43175479+levonw18@users.noreply.github.com>
2018-09-11 10:12:46 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_LevonWilson.py
@@ -0,0 +1,8 @@
+# Converting celcius to fahreheit converter
+# 09/10/18
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Levon Wilson
+#
+celcius = float(input("Input tempature in celcius"))
+fahreheit = celcius*9/5+32
+print("the tempature in Fahreheit is {}".format(fahreheit))
