22e1910de5a84e6e195b8a3bcbe35b01abba3690
levonw18 <43175479+levonw18@users.noreply.github.com>
2018-09-11 10:11:03 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_LevonWilson.py
@@ -0,0 +1,17 @@
+# Calulating sales prediction
+# 9/6/18
+# CTI-110 P2T1 - Sales Prediction
+# Levon Wilson
+#
+# Get the projeted total sales.
+total_sales= float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 perent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('the profit is $', format(profit,',.2f'))
+
+
+                   
+
